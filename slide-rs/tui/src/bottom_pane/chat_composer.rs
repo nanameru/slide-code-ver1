734ea0f9a41d05aff7171d47dfb948f8e5960165
nanameru <4869nanataitai@gmail.com>
2025-09-16 03:21:16 +0900

    tui: keep input border orange regardless of focus

--- slide-rs/tui/src/bottom_pane/chat_composer.rs
+++ slide-rs/tui/src/bottom_pane/chat_composer.rs
@@ -254,13 +254,8 @@ impl WidgetRef for &ChatComposer {
         ])
         .areas(area);
 
-        // Left border
-        let border_style = if self.has_focus {
-            // Use orange for focused input border
-            Style::default().fg(Color::Rgb(255, 165, 0))
-        } else {
-            Style::default().add_modifier(Modifier::DIM)
-        };
+        // Left border: always orange regardless of focus
+        let border_style = Style::default().fg(Color::Rgb(255, 165, 0));
 
         Block::default()
             .borders(Borders::LEFT)
